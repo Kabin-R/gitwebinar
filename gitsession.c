2e7117fb0cf0eb644119815f5479367a4d1576f0
Kabin Rimal <kabinrimal@gmail.com>
2021-07-18 12:36:10 +0545

    count function implemented

--- gitsession.c
+++ gitsession.c
@@ -3,5 +3,10 @@ int main()
 {
     int a=5,b=3,sum=0;
     sum=a+b;
-    printf("the sum is %d",sum);
+    printf("the sum is %d\n",sum);
+    for (size_t i = 1; i <= 10; i++)
+    {
+        printf("%d\t",i);
+    }
+    
 }
