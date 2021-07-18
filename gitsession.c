7c894df1a1708a71026a9923abb890d7ae0311b1
Kabin Rimal <kabinrimal@gmail.com>
2021-07-18 12:45:15 +0545

    switch is created

--- gitsession.c
+++ gitsession.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 int main()
-{
-    int a=5,b=3,sum=0;
-    sum=a+b;
-    printf("the sum is %d\n",sum);
-    for (size_t i = 1; i <= 10; i++)
+{   
+    int a=5,b=3,ans,sum=0;
+    printf("Choose one program to exicute\n\
+    1. sum of two number\n\
+    2. Count 1 to 10");
+    scanf("%d",&ans);
+    switch (ans)
     {
-        printf("%d\t",i);
+    case 1:
+        sum = a + b;
+        printf("the sum is %d\n", sum);
+        break;
+    
+    case 2:
+        for (size_t i = 1; i <= 10; i++)
+        {
+            printf("%d\t", i);
+        }
+        break;
     }
+    
     
 }
